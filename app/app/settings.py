35813eca87aa25083e4b95f51c7652eecf367d40
alexlopesbr <criabiobr@gmail.com>
2023-01-17 19:44:48 -0300

    start a new django project

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=s1h5ui(l2gbr_5dvnk!$@epm=#9*3&)g*2$99%c6kj)ev^%^k'
+SECRET_KEY = 'django-insecure-+z(c4tvke=albkjkp)r^ro%y@vq-@-vkixn-co1vc!1+=3o%f$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
